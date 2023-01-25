236b9e7edd26c24ca68b4ceffb915eb3a98c370c
CASSANDRA <CASSANDRA@LAPTOP-1GCEE8TH>
2023-01-25 21:45:39 +0800

    Add project files.

---
+++ Student_List/Data/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Student_List.Models;
+
+namespace Student_List.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder) 
+        {
+            modelBuilder.Entity<Student>();
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
---
+++ Student_List/Models/Student.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Student_List.Models
+{
+    public class Student
+    {
+        public string FullName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string Bio { get; set; }      
+    }
+}
